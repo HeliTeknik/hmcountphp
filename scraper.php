7bf02ea6d467fd45ffb212ece46906803cbefcb3
HeliTeknik <HeliTeknik@users.noreply.github.com>
2017-02-15 19:18:18 +0200

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,24 +1,18 @@
 <?php
 require 'scraperwiki.php';
-// A scraper for airline information from PHL.org
+    require 'scraperwiki/simple_html_dom.php';
+?>
 
-require 'scraperwiki/simple_html_dom.php'; 
-$html_content = scraperwiki::scrape("http://www.hm.com/gb/subdepartment/sale?Nr=4294944772"); 
-$html = str_get_html($html_content);
 
-foreach ($html->find("dl.settings dd") as $el) {  }
+<?php
+$url="http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm";
+$html = scraperWiki::scrape( $url );
+$dom = new simple_html_dom();
+$dom->load($html);
 
-$record = array( 'hmcount1' => $el->innertext, 'hmcount2' => $el->innertext);
-//$record = ( 'hmcount1' => $el->innertext);
-
-print $record . "\n";
- print json_encode($record) . "\n";
-
-//scraperwiki::save('hmcount1', $record); 
-//scraperwiki::sqliteexecute("delete from swdata"); 
-scraperwiki::sqliteexecute("drop table if exists swdata"); 
-//scraperwiki::sqliteexecute("drop table if exists hmcount_new"); 
-
-scraperwiki::save_var('last_page', (integer)$el->innertext); print scraperwiki::get_var('last_page');
+$record = array(
+            'html' => $tds[0]->plaintext 
+        );
+ scraperwiki::save( array('html'), $record )
 
 ?>
